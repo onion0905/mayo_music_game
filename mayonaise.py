4fa505cc5f7c7294b1dfa6d13c31f9fadd409142
onion1007 <onionchen0905@gmail.com>
2021-12-12 22:01:36 +0800

    still needed to be fixed
    the note displaying still needed to be fixed

--- mayonaise.py
+++ mayonaise.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import time
 pygame.init()
 
 # Screen
@@ -29,11 +30,53 @@ display_pressed3 = pygame.Rect(400, 500, slot[0], slot[1])
 display_pressed4 = pygame.Rect(525, 500, slot[0], slot[1])
 music = "D:\\program project\\python_project\\Games\\mayo_music_game\\images\\ver.hard.mp3"
 
+# Notes
+'''
+Time format:
+ex: 0005.00
+first four digits are for second
+last two digits are for milisecond
+'''
+
+times_arrive = []
+times_drop = []
+notes = []
+with open("D:\\program project\\python_project\\Games\\mayo_music_game\\times.txt", "r") as time_f:
+    for i in time_f:
+        i = i
+        times_arrive.append(i)
+
+with open("D:\\program project\\python_project\\Games\\mayo_music_game\\notes.txt", "r") as note_f:
+    for i in note_f:
+        notes.append(i)
+        print(i)
+        print(type(i))
+
+for i in times_arrive:
+    a = float(i) -1
+    lena = a//10
+    lena = int(lena)
+    b = ""
+    for j in range(3 - lena):
+        b += "0"
+    b += str(a)
+    times_drop.append(b)
+
+# Functions
+locations = [160, 285, 410, 535]
+def show_mayo(note):
+    for i in range(len(note)):
+        if note[i] == '1':
+            wn.blit(mayo, (locations[i], 50))
+            print("blitted")
+
 
 # Main process
 running = True
 back = 0
 mouse = ""
+pointer = 0
+start_time = 0
 while running:
     mouse_pos = pygame.mouse.get_pos()
     # background displaying
@@ -57,6 +100,7 @@ while running:
                 track = pygame.mixer.music.load(music)
                 pygame.mixer.music.set_volume(0.3)
                 pygame.mixer.music.play()
+                start_time = time.time()
 
     # pressed key displaying
     keys = pygame.key.get_pressed()
@@ -69,6 +113,18 @@ while running:
         pygame.draw.rect(wn, (99, 170, 219), display_pressed3)
     if keys[pygame.K_k]:
         pygame.draw.rect(wn, (99, 170, 219), display_pressed4)
+
+    # Notes displaying
+    now_time = time.time()
+    time_pass = float(now_time - start_time)
+    #print(time_pass)
+    #print(float(times_drop[pointer]))
+    #print(notes[pointer])
+    #print(type(notes[pointer]))
+    if time_pass <= float(times_drop[pointer])+0.1 and time_pass >= float(times_drop[pointer])-0.1:
+        show_mayo(notes[pointer])
+        wn.blit(mayo, (160, 50))
+        time.sleep(5)
     
     
     # pygame events
